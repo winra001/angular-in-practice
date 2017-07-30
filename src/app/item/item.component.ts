553fe3d21064891d595324361b48f9cb97bf8995
daniel ra <winra001@hotmail.com>
2017-07-30 21:28:50 +1000

    Section 5: Angular in Practice - Starting the Course Project

---
+++ src/app/item/item.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  @Input()
+  character;
+
+  @Output()
+  sideAssigned = new EventEmitter<{name: string, side: string}>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAssign(side) {
+    // this.character.side = side;
+    this.sideAssigned.emit({name: this.character.name, side: side});
+  }
+
+}
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  @Input()
+  characters;
+
+  @Output()
+  sideAssigned = new EventEmitter<{name: string, side: string}>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSideAssigned(charInfo) {
+    this.sideAssigned.emit(charInfo);
+  }
+
+}
