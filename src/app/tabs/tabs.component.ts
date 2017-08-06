0ec8addf1d836123e4822a31b208675937359932
daniel ra <winra001@hotmail.com>
2017-08-06 14:31:46 +1000

    Section 8: Changing Pages with Routing (83. Cleanup & Missing Reactivity)

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { StarWarsService } from "../star-wars.service";
 
 @Component({
   selector: 'app-tabs',
@@ -8,24 +7,9 @@ import { StarWarsService } from "../star-wars.service";
 })
 export class TabsComponent implements OnInit {
 
-  characters = [];
-  chosenList = 'all';
-  starWarsService: StarWarsService;
-
-  constructor(starWarsService: StarWarsService) {
-    this.starWarsService = starWarsService;
-  }
+  constructor() {}
 
   ngOnInit() {
-  }
-
-  onChoose(side: string) {
-    this.chosenList = side;
-  }
-
-  getCharacters() {
-    this.characters = this.starWarsService.getCharacters(this.chosenList);
-    return this.characters;
   }
 
 }
