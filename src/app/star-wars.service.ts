5e0bf56f07b649f54a633b1548c4073f955de354
daniel ra <winra001@hotmail.com>
2017-08-07 22:34:27 +1000

    Section 10: Connecting to APIs via Http (97. Transforming Responses)

--- src/app/star-wars.service.ts
+++ src/app/star-wars.service.ts
@@ -1,7 +1,8 @@
-import { LogService } from "./log.service";
-import { Injectable } from "@angular/core";
-import { Subject } from "rxjs/Subject";
-import { Http, Response } from "@angular/http";
+import { LogService } from './log.service';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class StarWarsService {
@@ -23,10 +24,20 @@ export class StarWarsService {
   }
 
   fetchCharacters() {
-    this.http.get('https://swapi.co/api/people/').subscribe(
-      (response: Response) => {
-        console.log(response);
-      }
+    this.http.get('https://swapi.co/api/people/')
+      .map((response: Response) => {
+        const data = response.json();
+        const extractedChars = data.results;
+        const chars = extractedChars.map((char) => {
+          return {name: char.name, side: ''};
+        });
+        return chars;
+      })
+      .subscribe(
+        (data) => {
+          console.log(data);
+          this.characters = data;
+        }
     );
   }
 
