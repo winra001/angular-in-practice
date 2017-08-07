ec754cc5a5e8db164dfae4c63af9df1413e0235c
daniel ra <winra001@hotmail.com>
2017-08-07 22:43:07 +1000

    Section 10: Connecting to APIs via Http (98. Updating UI)

--- src/app/star-wars.service.ts
+++ src/app/star-wars.service.ts
@@ -37,6 +37,7 @@ export class StarWarsService {
         (data) => {
           console.log(data);
           this.characters = data;
+          this.characterChanged.next();
         }
     );
   }
